164e7d49f1bac0c2d0f266955c24314ec4cf7575
Witex15 <noe.zwissig@bluewin.ch>
2023-10-02 20:05:16 +0200

    feat: creat getAllExercise function

--- model/exercise.php
+++ model/exercise.php
@@ -1,8 +1,7 @@
 <?php
-
-require_once('./databaseConnection.php');
-
-function getAllExercises(){
-    return dbConnect()->query("SELECT title_exercise FROM exercises");
-
+class exercise
+{
+    function getAllExercises(){
+        return DatabaseConnection::dbConnect()->query("SELECT title_exercise FROM exercises")->fetchAll();
+    }
 }

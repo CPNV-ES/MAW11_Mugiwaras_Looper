b4ce63a40e4752e25d98a568c723d6f56f4862b0
Witex15 <noe.zwissig@bluewin.ch>
2023-10-02 20:04:38 +0200

    feat: add dbConnect function

--- model/DatabaseConnection.php
+++ model/DatabaseConnection.php
@@ -2,4 +2,8 @@
 
 class DatabaseConnection
 {
+    public static function dbConnect()
+    {
+        return new PDO(getenv('PDO_DSN', true), getenv('PDO_USERNAME', true), getenv('PDO_PASSWORD', true));
+    }
 }

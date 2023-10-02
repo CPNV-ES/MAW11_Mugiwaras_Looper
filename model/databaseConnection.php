227239f19418040bb626d8dcaa80481a3e011aaa
Witex15 <noe.zwissig@bluewin.ch>
2023-10-02 20:06:12 +0200

    fix: databaseConnection fix

--- model/databaseConnection.php
+++ model/databaseConnection.php
@@ -1,7 +1,9 @@
 <?php
-function dbConnect()
-{
-    echo("toto");
-    return new PDO(getenv('PDO_DSN', true), getenv('PDO_USERNAME', true), getenv('PDO_PASSWORD', true));
 
+class DatabaseConnection
+{
+    public static function dbConnect()
+    {
+        return new PDO(getenv('PDO_DSN', true), getenv('PDO_USERNAME', true), getenv('PDO_PASSWORD', true));
+    }
 }

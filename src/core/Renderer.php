a7b7fd33bb447ad42f81ac944d160f570b321670
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-09 19:12:39 +0200

    fix: refactor Renderer

--- src/core/Renderer.php
+++ src/core/Renderer.php
@@ -1,28 +1,27 @@
 <?php
 
-namespace App\Renderer;
+namespace App\core;
 
 class Renderer {
     private $viewPath;
     private $layout = 'layout';  // default layout file
 
+    // Construct the final view
     public function __construct($viewPath) {
         $this->viewPath = $viewPath;
     }
 
+    // Render the view with the view and the layout
     public function render($view, $data = []) {
         $content = $this->getViewContent($view, $data);
         return $this->getViewContent($this->layout, ['content' => $content]);
     }
 
+    // Get the view to inject into the layout
     private function getViewContent($view, $data = []) {
         extract($data);
         ob_start();
         include $this->viewPath . '/' . $view . '.php';
         return ob_get_clean();
-    }
-
-    public function setLayout($layout) {
-        $this->layout = $layout;
     }
 }

d0d3b387d73c04ebf439497f9281ca4ba4aab200
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-10 19:31:43 +0200

    fix: view for answering

--- src/views/answering.php
+++ src/views/answering.php
@@ -1,15 +1,12 @@
-<?php
-/** @var array $exercises */
-?>
 <main class="container">
     <ul class="answering-list">
-            <?php foreach ($exercises as $exercise): ?>
-                <li class="row">
-                    <div class="column card">
-                        <div class="title"><?= htmlspecialchars($exercise['title_exercise']) ?></div>
-                        <a class="button" href="/exercises/<?= htmlspecialchars($exercise['id']) ?>">Take it</a>
-                    </div>
-                </li>
-            <?php endforeach; ?>
+        <?php foreach ($exercises as $exercise): ?>
+            <li class="row">
+                <div class="column card">
+                    <div class="title"><?= htmlspecialchars($exercise['title_exercise']) ?></div>
+                    <a class="button" href="/exercises/<?= htmlspecialchars($exercise['id']) ?>">Take it</a>
+                </div>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </main>

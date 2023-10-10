fad1ddf06b5716bd8429cd9ff0608e085f1ba4fb
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-10 19:38:52 +0200

    fix: update the path of the buttons

--- src/views/answering.php
+++ src/views/answering.php
@@ -4,7 +4,7 @@
             <li class="row">
                 <div class="column card">
                     <div class="title"><?= htmlspecialchars($exercise['title_exercise']) ?></div>
-                    <a class="button" href="/exercises/<?= htmlspecialchars($exercise['id']) ?>">Take it</a>
+                    <a class="button" href="/exercises/<?= htmlspecialchars($exercise['id']) ?>/fulfillments/new">Take it</a>
                 </div>
             </li>
         <?php endforeach; ?>

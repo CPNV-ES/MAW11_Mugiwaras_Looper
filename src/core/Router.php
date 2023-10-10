8c667ebbafe50319dd25e67c8076a6301cca9dd1
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-10 19:45:28 +0200

    fix: comment the router class

--- src/core/Router.php
+++ src/core/Router.php
@@ -14,15 +14,19 @@ class Router {
 
     public function dispatch($url) {
         foreach ($this->routes as $pattern => $route) {
+
+            // Checking if the pattern matches the URL
             if (preg_match("~^$pattern$~", $url, $matches)) {
                 array_shift($matches);
+
+                // If there is a match, call the controller and action
                 $controllerClass = "App\\Controllers\\" . $route['controller'];
                 $controller = new $controllerClass();
                 call_user_func_array([$controller, $route['action']], $matches);
                 return;
             }
         }
-
+        // If there is no match, call the error controller
         $errorController = new ErrorController();
         $errorController->notFound();
     }

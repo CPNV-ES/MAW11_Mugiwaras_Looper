9a5983d5622d83c4840ed1e652b3525c38a8bc81
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-10 20:48:45 +0200

    feat: create a separated header to inject into answering.php

---
+++ src/views/headers/answeringHeader.php
@@ -0,0 +1,9 @@
+<?php
+$dynamicHeader = '
+    <header class="container">
+    <section class="container">
+        <a href="/"><img src="/img/logo.png" alt="Looper"></a>
+    </section>
+</header>
+';
+?>

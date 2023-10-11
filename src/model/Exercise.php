812e7feb219d90ef1640808b7ac621b8af6358fe
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-11 10:31:40 +0200

    fix: change the method in order to get the titles and ids

--- src/model/Exercise.php
+++ src/model/Exercise.php
@@ -6,8 +6,10 @@ use App\Model\DatabaseConnection;
 
 class Exercise
 {
-    function getAllExercisesTitle()
+    function getAllExercises()
     {
-        return DatabaseConnection::dbConnect()->query("SELECT title_exercise FROM exercises")->fetchAll();
+        // Get the exercises from the database (titles and ids)
+        return DatabaseConnection::dbConnect()->query("SELECT id_exercise, title_exercise FROM exercises")->fetchAll();
     }
+
 }

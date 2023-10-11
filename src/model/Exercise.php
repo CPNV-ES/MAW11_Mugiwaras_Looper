bc2373c481547d1e008c68a999e0e30c2b65993b
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-11 14:38:47 +0200

    fix: change the connection method in order to be called only once on a specific model

--- src/model/Exercise.php
+++ src/model/Exercise.php
@@ -6,11 +6,17 @@ use App\Model\DatabaseConnection;
 
 class Exercise
 {
-    function getAllExercises()
+    private $db;
+
+    // DbConnect in the constructor will execute the connection once for every method in this class
+    function __construct()
+    {
+        $this->db = DatabaseConnection::dbConnect();
+    }
     function getAllExercises(): array|false
     {
         // Get the exercises from the database (titles and ids)
-        return DatabaseConnection::dbConnect()->query("SELECT id_exercise, title_exercise FROM exercises")->fetchAll();
+        return $this->db->query("SELECT id_exercise, title_exercise FROM exercises")->fetchAll();
     }
 
 }

eb98d262bf759fcc96dca05ef8bb1288f1bcaaad
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-11 09:47:03 +0200

    Fix: combining the route.php in router

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\core\Route;
+use App\core\Router;
 
 define('APP_ROOT', dirname(__DIR__));
 
@@ -8,8 +8,5 @@ require APP_ROOT . '/vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(APP_ROOT)->load();
 
-$route = new Route();
-$router = $route->getRouter();
-
-$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$router->dispatch($url);
+$router = new Router();
+$router->dispatch($_SERVER['REQUEST_URI']);

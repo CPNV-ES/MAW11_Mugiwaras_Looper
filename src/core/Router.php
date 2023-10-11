57643e4b911d2edd662afb8d89cb9ff187e852d6
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-11 14:37:13 +0200

    refactor: update the return type

--- src/core/Router.php
+++ src/core/Router.php
@@ -6,24 +6,27 @@ use App\Controllers\ErrorController;
 
 
 class Router {
-    private $routes = [];
+    private array $routes = [];
 
     public function __construct() {
         $this->addDefaultRoutes();
     }
 
-    private function addDefaultRoutes() {
+    private function addDefaultRoutes(): void
+    {
         $this->addRoute('/', 'HomeController', 'index');
         $this->addRoute('/exercises', 'ExerciseController', 'index');
         $this->addRoute('/exercises/answering', 'ExerciseController', 'answering');
         $this->addRoute('/exercises/new', 'ExerciseController', 'new');
-        $this->addRoute('/exercises', 'ExerciseController', 'manage'); // I assume you wanted /exercises/manage here
+        $this->addRoute('/exercises', 'ExerciseController', 'manage');
         // Add more routes as needed...
     }
-    public function addRoute($pattern, $controller, $action) {
+    public function addRoute($pattern, $controller, $action): void
+    {
         $this->routes[$pattern] = ['controller' => $controller, 'action' => $action];
     }
-    public function dispatch($url) {
+    public function dispatch($url): void
+    {
         foreach ($this->routes as $pattern => $route) {
 
             // Checking if the pattern matches the URL

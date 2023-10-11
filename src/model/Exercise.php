4897142bf81671eaa287d040a65194cccc85533f
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-11 14:37:55 +0200

    refactor: update the return type

--- src/model/Exercise.php
+++ src/model/Exercise.php
@@ -7,6 +7,7 @@ use App\Model\DatabaseConnection;
 class Exercise
 {
     function getAllExercises()
+    function getAllExercises(): array|false
     {
         // Get the exercises from the database (titles and ids)
         return DatabaseConnection::dbConnect()->query("SELECT id_exercise, title_exercise FROM exercises")->fetchAll();

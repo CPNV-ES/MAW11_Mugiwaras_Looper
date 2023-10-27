f156b07fa14a8f6f0aa7f0b82a38110df21d65ca
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-27 13:19:54 +0200

    feat: create the dynamicHeader for the new exercise page

---
+++ src/views/headers/createExerciseHeader.php
@@ -0,0 +1,8 @@
+<?php
+$dynamicHeader = '
+<header class="heading creating">
+    <section class="container">
+        <a href="/"><img src="/img/logo.png"  alt=""/></a>
+        <span class="exercise-label">New exercise</span>
+    </section>
+</header>';

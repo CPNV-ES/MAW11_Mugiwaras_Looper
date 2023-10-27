e5aefa437e920308b29eaab8f7b0cb8772f9bfa2
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-27 13:19:16 +0200

    feat: create the new exercise view

--- src/views/createExercise.php
+++ src/views/createExercise.php
@@ -1,4 +1,21 @@
-<?php require 'headers/answeringHeader.php'; ?>
-<div class="container dashboard">
-Future create exercise page
-</div>
+<?php require 'headers/createExerciseHeader.php'; ?>
+<body>
+<main class="container">
+    <h1>New Exercise</h1>
+
+    <form action="/exercises/new/fulfillment" accept-charset="UTF-8" method="post">
+
+
+        <div class="field">
+            <label for="exercise_title">Title</label>
+            <input type="text" name="exercise[title]" id="exercise_title" />
+        </div>
+
+        <div class="actions">
+            <input type="submit" name="commit" value="Create Exercise" data-disable-with="Create Exercise" />
+        </div>
+    </form>
+</main>
+
+</body>
+

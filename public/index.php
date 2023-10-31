a40b68d21cec83de592115b14e127851c80c0d6f
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-10-31 19:59:08 +0100

    refactor: one liner code to instance a new router

--- public/index.php
+++ public/index.php
@@ -8,5 +8,4 @@ require APP_ROOT . '/vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(APP_ROOT)->load();
 
-$router = new Router();
-$router->dispatch($_SERVER['REQUEST_URI']);
+(new \App\core\Router())->dispatch($_SERVER['REQUEST_URI']);

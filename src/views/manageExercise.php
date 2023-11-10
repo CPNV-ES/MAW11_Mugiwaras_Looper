ba730d6585b7fb943244305090220bab4f69f7c9
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-10 08:41:54 +0100

    feat: new manage page without the loops

--- src/views/manageExercise.php
+++ src/views/manageExercise.php
@@ -1,4 +1,27 @@
-<?php require 'headers/answeringHeader.php'; ?>
+<?php require 'headers/manageHeader.php'; ?>
+
 <div class="container dashboard">
-Future manage exercise page
+    <!-- Building Exercises Section -->
+    <section class="column">
+        <h1>Building</h1>
+        <table class="records">
+            <!-- PHP code to loop through 'Building' exercises and display them -->
+        </table>
+    </section>
+
+    <!-- Answering Exercises Section -->
+    <section class="column">
+        <h1>Answering</h1>
+        <table class="records">
+            <!-- PHP code to loop through 'Answering' exercises and display them -->
+        </table>
+    </section>
+
+    <!-- Closed Exercises Section -->
+    <section class="column">
+        <h1>Closed</h1>
+        <table class="records">
+            <!-- PHP code to loop through 'Closed' exercises and display them -->
+        </table>
+    </section>
 </div>

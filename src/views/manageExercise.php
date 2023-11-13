12f000881e40c0e620ca5e309b9dd636a9dec4a9
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-13 17:35:16 +0100

    feat: new feature front end page to display all exercises categorized

--- src/views/manageExercise.php
+++ src/views/manageExercise.php
@@ -1,27 +1,41 @@
 <?php require 'headers/manageHeader.php'; ?>
-
-<div class="container dashboard">
-    <!-- Building Exercises Section -->
-    <section class="column">
-        <h1>Building</h1>
-        <table class="records">
-            <!-- PHP code to loop through 'Building' exercises and display them -->
-        </table>
-    </section>
-
-    <!-- Answering Exercises Section -->
-    <section class="column">
-        <h1>Answering</h1>
-        <table class="records">
-            <!-- PHP code to loop through 'Answering' exercises and display them -->
-        </table>
-    </section>
-
-    <!-- Closed Exercises Section -->
-    <section class="column">
-        <h1>Closed</h1>
-        <table class="records">
-            <!-- PHP code to loop through 'Closed' exercises and display them -->
-        </table>
-    </section>
+<main class="container">
+    <!DOCTYPE html>
+    <html>
+<div class="row results">
+    <?php foreach (['Building', 'Answering', 'Closed'] as $status): ?>
+        <section class="column">
+            <h1><?= htmlspecialchars($status) ?></h1>
+            <table class="records">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if (!empty($categorizedExercises[$status])): ?>
+                    <?php foreach ($categorizedExercises[$status] as $exercise): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($exercise['title_exercise']) ?></td>
+                            <td>
+                                <a title="Manage fields" href="/exercises/<?= $exercise['id_exercise'] ?>/fields"><i class="fa fa-edit"></i></a>
+                                <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/<?= $exercise['id_exercise'] ?>"><i class="fa fa-trash"></i></a>
+                                <?php if ($status != 'Closed'): ?>
+                                    <a title="Be ready for answers" rel="nofollow" data-method="put" href="/exercises/<?= $exercise['id_exercise'] ?>?exercise%5Bstatus%5D=answering"><i class="fa fa-comment"></i></a>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="2">No exercises in <?= htmlspecialchars($status) ?> status.</td>
+                    </tr>
+                <?php endif; ?>
+                </tbody>
+            </table>
+        </section>
+    <?php endforeach; ?>
 </div>
+    </html>
+</main>

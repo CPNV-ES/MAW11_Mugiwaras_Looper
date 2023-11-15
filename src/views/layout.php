c50c59f90a8ace2923a297ece182692dfda2fc8a
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-15 08:29:05 +0100

    refactor: change the layout in order to separate tha call of the styles sheets

--- src/views/layout.php
+++ src/views/layout.php
@@ -1,22 +1,13 @@
+<?php require 'headers/layoutHead.php'; ?>
 <!DOCTYPE html>
-<html lang="fr">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?? "Exercise Looper" ?></title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
+    <title>Exercise Looper</title>
+    <?= $layoutHead ?? '' ?>
+    <script src="/js/manageExercises.js"></script>
+ </head>
     <body>
-
-        <!-- Header injection -->
         <?= $dynamicHeader ?? '' ?>
-            <!-- Main content injection -->
         <?= $content ?? '' ?>
-
     </body>
-
 </html>

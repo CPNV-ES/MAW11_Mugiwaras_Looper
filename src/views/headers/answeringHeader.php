f689ab7419a32975ab81fe69955c66f74514269f
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-15 08:36:53 +0100

    refactor: change the class to be same as the css file

--- src/views/headers/answeringHeader.php
+++ src/views/headers/answeringHeader.php
@@ -1,6 +1,6 @@
 <?php
 $dynamicHeader = '
-    <header class="container">
+    <header class="heading answering">
     <section class="container">
         <a href="/"><img src="/img/logo.png" alt="Looper"></a>
     </section>

a5ffc214f9a1d5d37f99ad5627a873e7af90625f
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-15 08:38:00 +0100

    feat: the new manage page with all exercises categorized

--- src/views/manageExercise.php
+++ src/views/manageExercise.php
@@ -2,40 +2,42 @@
 <main class="container">
     <!DOCTYPE html>
     <html>
-<div class="row results">
-    <?php foreach (['Building', 'Answering', 'Closed'] as $status): ?>
-        <section class="column">
-            <h1><?= htmlspecialchars($status) ?></h1>
-            <table class="records">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php if (!empty($categorizedExercises[$status])): ?>
-                    <?php foreach ($categorizedExercises[$status] as $exercise): ?>
-                        <tr>
-                            <td><?= htmlspecialchars($exercise['title_exercise']) ?></td>
-                            <td>
-                                <a title="Manage fields" href="/exercises/<?= $exercise['id_exercise'] ?>/fields"><i class="fa fa-edit"></i></a>
-                                <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/<?= $exercise['id_exercise'] ?>"><i class="fa fa-trash"></i></a>
-                                <?php if ($status != 'Closed'): ?>
-                                    <a title="Be ready for answers" rel="nofollow" data-method="put" href="/exercises/<?= $exercise['id_exercise'] ?>?exercise%5Bstatus%5D=answering"><i class="fa fa-comment"></i></a>
-                                <?php endif; ?>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else: ?>
+    <div class="row">
+        <?php foreach (['Building', 'Answering', 'Closed'] as $status): ?>
+            <section class="column">
+                <h1><?= htmlspecialchars($status) ?></h1>
+                <table class="records">
+                    <thead>
                     <tr>
-                        <td colspan="2">No exercises in <?= htmlspecialchars($status) ?> status.</td>
+                        <th>Title</th>
+                        <th></th>
                     </tr>
-                <?php endif; ?>
-                </tbody>
-            </table>
-        </section>
-    <?php endforeach; ?>
-</div>
+                    </thead>
+                    <tbody>
+                    <?php if (!empty($categorizedExercises[$status])): ?>
+                        <?php foreach ($categorizedExercises[$status] as $exercise): ?>
+                            <tr>
+                                <td><?= htmlspecialchars($exercise['title_exercise']) ?></td>
+                                <td>
+                                    <a title="Manage fields" href="/exercises/<?= $exercise['id_exercise'] ?>/fields"><i class="fa fa-edit"></i></a>
+                                    <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/<?= $exercise['id_exercise'] ?>"><i class="fa fa-trash"></i></a>
+                                    <?php if ($status != 'Closed' && $status != 'Answering'): ?>
+                                        <a href="/exercises/updateStatus?id_exercise=1&newStatus=Answering" class="status-update-link" data-id="<?= $exercise['id_exercise'] ?>" data-status="Answering">
+                                            <i class="fa fa-comment"></i>
+                                        </a>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <tr>
+                            <td colspan="2">No exercises in <?= htmlspecialchars($status) ?> status.</td>
+                        </tr>
+                    <?php endif; ?>
+                    </tbody>
+                </table>
+            </section>
+        <?php endforeach; ?>
+    </div>
     </html>
 </main>

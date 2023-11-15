2232310ac0d046154c7b686e7ffe9e7e359cfaef
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-11-15 08:27:36 +0100

    feat : display the exercise name bolded

--- src/views/newFields.php
+++ src/views/newFields.php
@@ -1,4 +1,7 @@
 <?php require 'headers/createExerciseHeader.php'; ?>
 <div class="container dashboard">
     Future new field exercise page
-</div>
+</div>        <a href="/"><img src="/img/logo.png"  alt=""/></a>
+        <span class="exercise-label">exercise : <strong class="bolded"><?= $data['exercise']['title_exercise'] ?></strong></span>
+    </section>
+</header>

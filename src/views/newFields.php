d6979672575f9ac657833c0547a526d79c0f8243
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-11-15 08:28:10 +0100

    feat : display all field that are link to the exercise

--- src/views/newFields.php
+++ src/views/newFields.php
@@ -5,3 +5,13 @@
         <span class="exercise-label">exercise : <strong class="bolded"><?= $data['exercise']['title_exercise'] ?></strong></span>
     </section>
 </header>
+                <?php foreach ($data['fields'] as $field): ?>
+                    <tr>
+                        <td><?= $field['label']?></td>
+                        <td><?= $field['value_kind']?></td>
+                        <td>
+                            <a title="Edit" href="/exercises/<?= $data['exercise']['id_exercise'] ?>/fields/<?= $field['id_field'] ?>/edit"><i class="fa fa-edit"></i></a>
+                            <a data-confirm="Are you sure?" title="Destroy" rel="nofollow" data-method="delete" href="/exercises/<?= $data['exercise']['id_exercise'] ?>/fields/<?= $field['id_field'] ?>"><i class="fa fa-trash"></i></a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>

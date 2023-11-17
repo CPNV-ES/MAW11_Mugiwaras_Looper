a31c1cc50fb99d9bdf930d94f689f5b4174dfe41
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-17 08:21:41 +0100

    refactor : correct code style to PSR12

--- src/controllers/ExercisesController.php
+++ src/controllers/ExercisesController.php
@@ -4,18 +4,23 @@ namespace App\controllers;
 
 use App\core\Renderer;
 use App\models\Exercise;
-class ExercisesController {
+
+class ExercisesController
+{
 
     private Exercise $model;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->model = new Exercise();
     }
+
     public function index(): void
     {
         $categorizedExercises = $this->model->getCategorizedExercises();
         Renderer::render("manageExercise", ['categorizedExercises' => $categorizedExercises]);
     }
+
     public function answering(): void
     {
         // Call the function getAllExercises() in order to send the titles and ids of the exercises to the view
@@ -23,10 +28,12 @@ class ExercisesController {
         $data = ['exercises' => $exercises];
         Renderer::render("answering", $data);
     }
+
     public function new(): void
     {
         Renderer::render("createExercise");
     }
+
     public function create(): void
     {
         // Get the title of the new exercise from the form
@@ -36,10 +43,12 @@ class ExercisesController {
         // Exercise creation succeeded, we redirect to the new exercise's page.
         header("Location: /exercises/$exerciseId/fields");
     }
+
     public function fields(): void
     {
         Renderer::render("newFields");
     }
+
     public function updateStatus(): void
     {
         $exerciseId = $_GET['id_exercise'] ?? null;

d2776ccd1180388167bba360028bfa79bbae0571
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-17 08:21:57 +0100

    refactor : correct code style to PSR12

--- src/models/DatabaseConnection.php
+++ src/models/DatabaseConnection.php
@@ -9,6 +9,5 @@ class DatabaseConnection
     public static function dbConnect(): PDO
     {
         return new PDO(getenv('PDO_DSN', true), getenv('PDO_USERNAME', true), getenv('PDO_PASSWORD', true));
-
     }
 }

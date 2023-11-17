baf97a0a1d085f125365e5730582192dbff7157e
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-11-17 08:22:24 +0100

    refactor : correct code style to PSR12

--- src/views/layout.php
+++ src/views/layout.php
@@ -1,13 +1,14 @@
-<?php require 'headers/layoutHead.php'; ?>
+<?php
+require 'headers/layoutHead.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Exercise Looper</title>
     <?= $layoutHead ?? '' ?>
     <script src="/js/manageExercises.js"></script>
- </head>
-    <body>
-        <?= $dynamicHeader ?? '' ?>
-        <?= $content ?? '' ?>
-    </body>
+</head>
+<body>
+<?= $dynamicHeader ?? '' ?>
+<?= $content ?? '' ?>
+</body>
 </html>

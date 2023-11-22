ca76f7a7a57a123ab160f8a1ed290890591e4039
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-11-22 10:12:34 +0100

    feat : add all route in index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\core\Router;
-
+use \Mugiwaras\Framework\Core\Route;
 define('APP_ROOT', dirname(__DIR__));
 
 require APP_ROOT . '/vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(APP_ROOT)->load();
 
-(new Router())->dispatch($_SERVER['REQUEST_URI']);
+$router = new \Mugiwaras\Framework\Core\Router([
+new Route("get","/","HomeController@index"),
+new Route("get","/exercises","ExercisesController@manage"),
+new Route("get","/exercises/new","ExercisesController@new"),
+new Route("get","/exercises/answering","ExercisesController@answering"),
+
+new Route("post","/exercises/create","ExercisesController@create"),
+new Route("get","/exercises/{exerciseId}/fields","ExercisesController@fields"),
+new Route("post","/exercises/{exerciseId}/fields","ExercisesController@createField"),
+new Route("get","/exercises/{exerciseId}/fulfillments/new","ExercisesController@fulfillments"),
+
+    ]);
+$router->run();

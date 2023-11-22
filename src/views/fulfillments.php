aea9c50d68ce15a372e868f38a2b74fda0235584
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-11-22 09:06:41 +0100

    feat: fulfillment page

--- src/views/fulfillments.php
+++ src/views/fulfillments.php
@@ -1,1 +1,27 @@
-<?php
+<header class="heading managing">
+    <section class="container">
+        <a href="/"><img src="/img/logo.png" alt=""/></a>
+        <span class="exercise-label">exercise : <strong class="bolded"><?= $data['exercise']['title_exercise'] ?></strong></span>
+    </section>
+</header>
+<body>
+<main class="container">
+    <h1>Your take</h1>
+    <p>If you'd like to come back later to finish, simply submit it with blanks</p>
+
+    <form action="/exercises/<?= $data['exercise']['id_exercise'] ?>/fulfillments" accept-charset="UTF-8" method="post">
+        <?php
+        foreach ($data['fields'] as $field): ?>
+        <div class="field">
+            <label for="fulfillment_answers_attributes__value"><?= $field['label'] ?></label>
+            <input type="<?= $field['value_kind'] ?>" name="fulfillment[answers_attributes][][value]" id="fulfillment_answers_attributes__value">
+        </div>
+        <?php
+        endforeach; ?>
+        <div class="actions">
+            <input type="submit" name="commit" value="Save" data-disable-with="Save">
+        </div>
+    </form>
+</main>
+
+</body>

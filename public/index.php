019c1af9be5b3cc40092871bd93b80d8f21393d5
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-12-01 08:51:47 +0100

    Change the index to work with the framework router

--- public/index.php
+++ public/index.php
@@ -1,13 +1,19 @@
 <?php
 
+<<<<<<< Updated upstream
 use App\core\Router;
 use \Mugiwaras\Framework\Core\Route;
+=======
+use \Mugiwaras\Framework\Core\Route;
+
+>>>>>>> Stashed changes
 define('APP_ROOT', dirname(__DIR__));
 
 require APP_ROOT . '/vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(APP_ROOT)->load();
 
+<<<<<<< Updated upstream
 $router = new \Mugiwaras\Framework\Core\Router([
 new Route("get","/","HomeController@index"),
 new Route("get","/exercises","ExercisesController@index"),
@@ -21,3 +27,20 @@ new Route("get","/exercises/{exerciseId}/fulfillments/new","ExercisesController@
 
     ]);
 $router->run();
+=======
+
+//$router = new Router();
+$router = new \Mugiwaras\Framework\Core\Router([
+    new Route('GET', '/', "HomeController@index"),
+    new Route('GET', '/exercises', "ExercisesController@index"),
+    new Route('GET', '/exercises/{id}/edit', "ExercisesController@fields"),
+    new Route('GET', '/exercises/{id}/results', "ExercisesController@showResults"),
+]);
+
+$url = $_SERVER['REQUEST_URI'];
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$router->run();
+//$router->dispatch($url, $requestMethod);
+>>>>>>> Stashed changes

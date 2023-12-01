3e08d66481e208f3442a1a4cc6e2e3fbd0deb95e
Philippe Moeckli <philippe.moeckli1998@gmail.com>
2023-12-01 08:45:43 +0100

    Implement the show results in order to have the results with checked mark or cross mark

--- src/controllers/ExercisesController.php
+++ src/controllers/ExercisesController.php
@@ -44,29 +44,59 @@ class ExercisesController
         header("Location: /exercises/$exerciseId/fields");
     }
 
-    public function fields(): void
+    public function fields(array $bag): void
     {
         $label = $_POST['field']['label'] ?? '';
         $fieldKind = $_POST['field']['value_kind'] ?? '';
         $exercise = $this->model->getLastInsertedExercise();
 
-        if(!empty($fieldKind)){
+        if (!empty($fieldKind)) {
             $this->model->addField($label, $fieldKind, $exercise[0]['id_exercise']);
         }
         $fields = $this->model->getFields($exercise[0]['id_exercise']);
 
         $data = ["exercise" => $exercise[0], "fields" => $fields];
-        Renderer::render("newFields",$data);
+        Renderer::render("newFields", $data);
     }
 
     public function updateStatus(): void
     {
         $exerciseId = $_GET['id_exercise'] ?? null;
         $newStatus = $_GET['newStatus'] ?? null;
-
         if ($exerciseId && $newStatus) {
             $this->model->updateExerciseStatus($exerciseId, $newStatus);
             header("Location: /exercises");
         }
+    }
+
+    public function showResults(array $data): void
+    {
+        $exerciseId = $data['id'];
+        $data = $this->model->getAnswersByFields($exerciseId);
+        $uniqueFields = $this->getUniqueFields($data);
+        $answersByFulfillment = $this->groupAnswersByFulfillment($data);
+        var_dump($answersByFulfillment);
+
+        Renderer::render("results", compact('uniqueFields', 'answersByFulfillment'));
+    }
+
+    private function getUniqueFields(array $data): array
+    {
+        return array_unique(array_column($data, 'field_label'));
+    }
+
+    private function groupAnswersByFulfillment(array $data): array
+    {
+        $answersByFulfillment = [];
+
+        foreach ($data as $row) {
+            $fulfilled_at = $row['fulfilled_at'];
+            $field_label = $row['field_label'];
+            $answer = $row['answer'];
+
+            $answersByFulfillment[$fulfilled_at][$field_label] = $answer;
+        }
+
+        return $answersByFulfillment;
     }
 }

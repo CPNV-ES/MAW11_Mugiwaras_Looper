4da7465b494bc90be5ed3d16648e085238098447
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-12-08 08:46:12 +0100

    add editfield view

---
+++ src/views/editField.php
@@ -0,0 +1,27 @@
+<?php
+require 'headers/exerciseNameHeader.php'; ?>
+<body>
+<main class="container">
+    <h1>New Exercise</h1>
+
+    <form action="/exercises/<?= $data['exercise']['id_exercise'] ?>/fields/<?= $data['field']['id_field'] ?>" accept-charset="UTF-8" method="post">
+        <div class="exercise-label">
+            <label for="label">Label</label>
+            <input type="text" name="fieldLabel" value="<?= $data['field']['label']?>" id="field_label"/>
+        </div>
+        <div class="field_kind">
+            <label for="label">Value kind</label>
+            <select name="fieldKind" id="field_kind">
+                <option <?= ($data['field']['value_kind'] == "single_line") ? 'selected="selected"' : ''; ?> value="single_line">Single line text</option>
+                <option <?= ($data['field']['value_kind'] == "single_line_list") ? 'selected="selected"' : ''; ?>value="single_line_list">List of single lines</option>
+                <option <?= ($data['field']['value_kind'] == "multi_line") ? 'selected="selected"' : ''; ?>value="multi_line">Multi-line text</option>
+            </select>
+        </div>
+        <div class="actions">
+            <input type="submit" value="Update field"/>
+        </div>
+    </form>
+</main>
+
+</body>
+

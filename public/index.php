134da4ba7a7e4771268ee18f8b902b683d0faef1
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-12-15 09:06:56 +0100

    correct index after merge

--- public/index.php
+++ public/index.php
@@ -35,19 +35,6 @@ $router = new \Mugiwaras\Framework\Core\Router([
     new Route("post", "/exercises/{exerciseId}/fulfillments/{fulfillmentId}/edit", "ExercisesController@updateAnswers"),
 
     new Route("get", "/exercises/{exerciseId}/results", "ExercisesController@showResults"),
-]);
-
-new Route("get","/","HomeController@index"),
-new Route("get","/exercises","ExercisesController@index"),
-new Route("get","/exercises/new","ExercisesController@new"),
-new Route("get","/exercises/answering","ExercisesController@answering"),
-
-new Route("post","/exercises/create","ExercisesController@create"),
-new Route("get","/exercises/{exerciseId}/fields","ExercisesController@fields"),
-new Route("post","/exercises/{exerciseId}/fields","ExercisesController@createField"),
-new Route("post","/exercises/{exerciseId}/fulfillments","ExercisesController@saveAnswers"),
-new Route("get","/exercises/{exerciseId}/fulfillments/new","ExercisesController@fulfillments"),
-new Route("get","/exercises/{exerciseId}/fulfillments/{fulfillmentId}/edit","ExercisesController@fulfillmentsEdit"),
     new Route("get", "/exercises/{exerciseId}/results/{fieldId}", "ResultController@getResultsOfField"),
     ]);
 $router->run();

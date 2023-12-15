8dda74a6f82a06b6cff1e876b2e2e36951acf38e
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2023-12-15 10:46:03 +0100

    update index

--- public/index.php
+++ public/index.php
@@ -16,27 +16,22 @@ $router = new \Mugiwaras\Framework\Core\Router([
     new Route("post", "/exercises", "ExercisesController@create"),
 
     new Route("get", "/exercises/new", "ExercisesController@new"),
-
     new Route("get", "/exercises/answering", "ExercisesController@answering"),
 
     new Route("put", "/exercises/{exerciseId}", "ExercisesController@updateStatus"),
-    //used ?
-    new Route('get', "/exercises/{id}/edit", "FieldsController@index"),
-    new Route('get', "/exercises/{exerciseId}/results", "ResultsController@index"),
     new Route("delete","/exercises/{exerciseId}","ExercisesController@destroy"),
 
+    new Route('get', "/exercises/{exerciseId}/results", "ResultsController@index"),
 
     new Route("get", "/exercises/{exerciseId}/fields", "FieldsController@index"),
     new Route("post", "/exercises/{exerciseId}/fields", "FieldsController@create"),
-
-    //change to put ?
     new Route("post", "/exercises/{exerciseId}/fields/{fieldId}", "FieldsController@update"),
     new Route("get", "/exercises/{exerciseId}/fields/{fieldId}/edit", "FieldsController@edit"),
     new Route("delete", "/exercises/{exerciseId}/fields/{fieldId}", "FieldsController@delete"),
 
-    new Route("get", "/exercises/{exerciseId}/fulfillments/new", "FulfillmentController@new"),
     new Route("get", "/exercises/{exerciseId}/fulfillments", "FulfillmentController@index"),
     new Route("post", "/exercises/{exerciseId}/fulfillments", "FulfillmentController@create"),
+    new Route("get", "/exercises/{exerciseId}/fulfillments/new", "FulfillmentController@new"),
     new Route("get", "/exercises/{exerciseId}/fulfillments/{fulfillmentId}", "FulfillmentController@show"),
     new Route("post", "/exercises/{exerciseId}/fulfillments/{fulfillmentId}", "FulfillmentController@update"),
     new Route("get", "/exercises/{exerciseId}/fulfillments/{fulfillmentId}/edit", "FulfillmentController@edit"),

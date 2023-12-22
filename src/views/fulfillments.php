f1a62d5c84d3d2e41903eb82fe2797fd1d00d0ea
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2023-12-22 10:06:00 +0100

    Add the textarea when the value kind is multiline

--- src/views/fulfillments.php
+++ src/views/fulfillments.php
@@ -8,14 +8,16 @@
     <p>If you'd like to come back later to finish, simply submit it with blanks</p>
 
     <form action="/exercises/<?= $exercise['id_exercise'] ?>/fulfillments" accept-charset="UTF-8" method="post">
-        <?php
-        foreach ($fields as $field): ?>
-        <div class="field">
-            <label for="fulfillment_answers_attributes_value"><?= $field['label'] ?: 'Value'  ?></label>
-            <input type="<?= $field['value_kind'] ?>" name="answer_<?=$field['id_field']?>" id="fulfillment_answers_attributes_value" >
-        </div>
-        <?php
-        endforeach; ?>
+        <?php foreach ($fields as $field): ?>
+            <div class="field">
+                <label for="fulfillment_answers_attributes_value"><?= $field['label'] ?: 'Value' ?></label>
+                <?php if ($field['value_kind'] === 'multi_line'): ?>
+                    <textarea name="answer_<?= $field['id_field'] ?>" id="fulfillment_answers_attributes_value"></textarea>
+                <?php else: ?>
+                    <input type="<?= $field['value_kind'] ?>" name="answer_<?= $field['id_field'] ?>" id="fulfillment_answers_attributes_value">
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
         <div class="actions">
             <input type="submit" value="Save" data-disable-with="Save">
         </div>
